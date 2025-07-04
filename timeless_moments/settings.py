d2756f9f0ab624bb07c036703c4b1131ea54374e
Nikesh Tamang <nikeshtamangghising@gmail.com>
2025-07-04 14:30:09 +0545

    Temporarily allow all hosts for Render deployment debugging

--- timeless_moments/settings.py
+++ timeless_moments/settings.py
@@ -17,7 +17,8 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='127.0.0.1,localhost', cast=Csv())
+# TEMPORARY: Allow all hosts for debugging on Render
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 INSTALLED_APPS = [
